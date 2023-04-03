da259e30d4df61fe7ad0e25bb4907027209c3e67
Luisa Horowitz <118489479+luhoro@users.noreply.github.com>
2023-04-03 16:51:56 -0300

    add files

---
+++ javascript/to-do-list/js/main.js
@@ -0,0 +1,39 @@
+
+const Main = {
+
+   init: function (){
+      this.cacheSelectors()
+      this.bindEvents()
+   },
+
+   cacheSelectors: function (){
+      this.$checkButtons = document.querySelectorAll('.check')
+   },
+
+   bindEvents: function (){
+      const self = this
+
+      this.$checkButtons.forEach(function(button) {
+         button.onclick = self.Events.checkButton_click
+      })
+   },
+
+
+
+   Events: {
+      checkButton_click: function(e){
+         const li = e.target.parentElement
+         const isDone = li.classList.contains('done')
+
+         if (!isDone) {
+            return li.classList.add('done')
+         }
+         
+         li.classList.add('done')
+      }
+   }
+
+
+}
+
+Main.init()
