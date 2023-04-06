1ae116e348b3980cbd99bba36dde090ff903a6f5
Luisa Horowitz <118489479+luhoro@users.noreply.github.com>
2023-04-05 21:16:32 -0300

    Add files via upload

--- javascript/to-do-list/js/main.js
+++ javascript/to-do-list/js/main.js
@@ -8,6 +8,9 @@ const Main = {
 
    cacheSelectors: function (){
       this.$checkButtons = document.querySelectorAll('.check')
+      this.$inputTask = document.querySelector('#inputTask')
+      this.$list = document.querySelector('#list')
+      this.$removeButtons = document.querySelectorAll('.remove')
    },
 
    bindEvents: function (){
@@ -15,6 +18,12 @@ const Main = {
 
       this.$checkButtons.forEach(function(button) {
          button.onclick = self.Events.checkButton_click
+      })
+
+      this.$inputTask.onkeypress = self.Events.inputTask_keypress.bind(this)
+
+      this.$removeButtons.forEach (function(button){
+         button.onclick = self.Events.removeButton_click
       })
    },
 
@@ -29,7 +38,39 @@ const Main = {
             return li.classList.add('done')
          }
          
-         li.classList.add('done')
+         li.classList.remove('done')
+      },
+
+      inputTask_keypress: function(e){
+         const key = e.key
+         const value = e.target.value
+
+         if (key === 'Enter') {
+            this.$list.innerHTML += `
+               <li >
+                  <div class="check"></div>
+                  <label for="" class="task">
+                     ${value}
+                  </label>
+                  <button class="remove"></button>
+               </li>
+            `
+
+            e.target.value = ''
+
+            this.cacheSelectors()
+            this.bindEvents()
+         }
+      },
+
+      removeButton_click: function(e){
+         let li = e.target.parentElement
+
+         li.classList.add('removed')
+
+         setTimeout(function(){
+            li.classList.add('hidden')
+         }, 300)
       }
    }
 
