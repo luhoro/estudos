3ec54b9d47f0aea58913b3c386b0d108c46fa8ea
Luisa Horowitz <118489479+luhoro@users.noreply.github.com>
2023-04-16 16:35:53 -0300

    Add files via upload

--- javascript/mochila-de-viagem/js/main.js
+++ javascript/mochila-de-viagem/js/main.js
@@ -1,7 +1,12 @@
-// variáveis para o formulário, o conjunto de elementos, e array vazio para armazenamento
+// variáveis para o formulário, o conjunto de elementos, e array para armazenamento dos itens
 const form = document.getElementById('novoItem')
 const lista = document.getElementById('lista')
-const itens = []
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+// percorrer a lista de itens e garantir a exibição ao atualizar página
+itens.forEach( (elemento) => {
+   criaElemento(elemento)
+})
 
 // Evento de envio do formulário
 form.addEventListener('submit', (evento) => {
@@ -9,8 +14,20 @@ form.addEventListener('submit', (evento) => {
 
    const nome = evento.target.elements['nome']
    const quantidade = evento.target.elements['quantidade']
+   
+   // cria o objeto do conjunto de informações de cada elemento
+   const itemAtual = {
+      "nome": nome.value,
+      "quantidade": quantidade.value
+   }
 
-   criaElemento(nome.value, quantidade.value)
+   criaElemento(itemAtual)
+
+   // enviando os valores de cada objeto para um array
+   itens.push(itemAtual)
+
+   // localstorage só lê elementos em string - stringify faz essa transformação para objeto
+   localStorage.setItem('itens', JSON.stringify(itens))
 
    // forma de resetar valores do formulário após envio
    nome.value = ''
@@ -18,30 +35,20 @@ form.addEventListener('submit', (evento) => {
 })
 
 // função para criação dos elementos
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
    // cria um elemento li com classe 'item
    const novoItem = document.createElement('li')
    novoItem.classList.add('item')
 
    //cria o elemento número de quantidade
    const numeroItem = document.createElement('strong')
-   numeroItem.innerHTML = quantidade
-   novoItem.appendChild(numeroItem)
+   numeroItem.innerHTML = item.quantidade
+   novoItem.appendChild(numeroItem) // anexa um elemento como filho de outro elemento
 
-   novoItem.innerHTML += nome
+   novoItem.innerHTML += item.nome
 
    lista.appendChild(novoItem)
 
-   // cria o objeto do conjunto de informações de cada elemento
-   const itemAtual = {
-      "nome": nome,
-      "quantidade": quantidade
-   }
-
-   // enviando os valores de cada objeto para um array
-   itens.push(itemAtual)
-
-   // localstorage só lê elementos em string - stringfy faz essa transformação
-   localStorage.setItem('item', JSON.stringify(itens))
+   
 }
 
