f6a1cd86843d6fed8bbd89be88ec740c5396a2a3
Luisa Horowitz <118489479+luhoro@users.noreply.github.com>
2023-04-17 22:31:09 -0300

    Add files via upload

--- javascript/mochila-de-viagem/js/main.js
+++ javascript/mochila-de-viagem/js/main.js
@@ -14,17 +14,27 @@ form.addEventListener('submit', (evento) => {
 
    const nome = evento.target.elements['nome']
    const quantidade = evento.target.elements['quantidade']
-   
-   // cria o objeto do conjunto de informações de cada elemento
-   const itemAtual = {
+
+   const existe = itens.find(elemento => elemento.nome === nome.value) // confere o elemento nome no array de itens
+
+   const itemAtual = { // cria o objeto do conjunto de informações de cada elemento
       "nome": nome.value,
       "quantidade": quantidade.value
    }
 
-   criaElemento(itemAtual)
+   if (existe) {  // determina se o item vai ser sobreposto ou não
+      itemAtual.id = existe.id
 
-   // enviando os valores de cada objeto para um array
-   itens.push(itemAtual)
+      atualizaElemento(itemAtual)
+      itens[existe.id] = itemAtual
+   } else {
+      itemAtual.id = itens.length
+
+      criaElemento(itemAtual)
+      itens.push(itemAtual)
+   }
+   
+   itens.push(itemAtual) // enviando os valores de cada objeto para um array
 
    // localstorage só lê elementos em string - stringify faz essa transformação para objeto
    localStorage.setItem('itens', JSON.stringify(itens))
@@ -34,21 +44,20 @@ form.addEventListener('submit', (evento) => {
    quantidade.value = ''
 })
 
-// função para criação dos elementos
-function criaElemento(item) {
-   // cria um elemento li com classe 'item
-   const novoItem = document.createElement('li')
+function criaElemento(item) { // função para criação dos elementos
+   const novoItem = document.createElement('li') // cria um elemento li com classe 'item
    novoItem.classList.add('item')
-
-   //cria o elemento número de quantidade
-   const numeroItem = document.createElement('strong')
-   numeroItem.innerHTML = item.quantidade
+   
+   const numeroItem = document.createElement('strong') //cria o elemento número de quantidade
+   numeroItem.innerHTML = item.quantidade // 
+   numeroItem.dataset.id = item.id
    novoItem.appendChild(numeroItem) // anexa um elemento como filho de outro elemento
 
    novoItem.innerHTML += item.nome
 
    lista.appendChild(novoItem)
-
-   
 }
 
+function atualizaElemento (item) { 
+   document.querySelector(`[data-id="${item.id}"]`).innerHTML = item.quantidade
+}
