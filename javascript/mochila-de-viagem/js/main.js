348757da0125cf86d836903b4c4f7eca491bd27d
Luisa Horowitz <118489479+luhoro@users.noreply.github.com>
2023-04-18 11:41:48 -0300

    Add files via upload

--- javascript/mochila-de-viagem/js/main.js
+++ javascript/mochila-de-viagem/js/main.js
@@ -3,7 +3,7 @@ const form = document.getElementById('novoItem')
 const lista = document.getElementById('lista')
 const itens = JSON.parse(localStorage.getItem("itens")) || []
 
-// percorrer a lista de itens e garantir a exibição ao atualizar página
+// percorre a lista de itens e garante a exibição ao atualizar página
 itens.forEach( (elemento) => {
    criaElemento(elemento)
 })
@@ -26,26 +26,24 @@ form.addEventListener('submit', (evento) => {
       itemAtual.id = existe.id
 
       atualizaElemento(itemAtual)
-      itens[existe.id] = itemAtual
+      itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
    } else {
-      itemAtual.id = itens.length
+      itemAtual.id = itens[itens.length - 1] ? itens[itens.length-1].id + 1 : 0
 
       criaElemento(itemAtual)
       itens.push(itemAtual)
    }
-   
-   itens.push(itemAtual) // enviando os valores de cada objeto para um array
 
    // localstorage só lê elementos em string - stringify faz essa transformação para objeto
    localStorage.setItem('itens', JSON.stringify(itens))
 
-   // forma de resetar valores do formulário após envio
+   // forma de resetar formulário do formulário após envio
    nome.value = ''
    quantidade.value = ''
 })
 
-function criaElemento(item) { // função para criação dos elementos
-   const novoItem = document.createElement('li') // cria um elemento li com classe 'item
+function criaElemento(item) { 
+   const novoItem = document.createElement('li') // cria um elemento li com classe item
    novoItem.classList.add('item')
    
    const numeroItem = document.createElement('strong') //cria o elemento número de quantidade
@@ -55,9 +53,30 @@ function criaElemento(item) { // função para criação dos elementos
 
    novoItem.innerHTML += item.nome
 
+   novoItem.appendChild(botatoDeleta(item.id))
+
    lista.appendChild(novoItem)
 }
 
 function atualizaElemento (item) { 
    document.querySelector(`[data-id="${item.id}"]`).innerHTML = item.quantidade
+}
+
+function botatoDeleta(id) { 
+   const elementoBotao = document.createElement('button')
+   elementoBotao.innerText = "⨯"
+
+   elementoBotao.addEventListener('click', function () { // com a arrow function não daria para usar o this
+      deletaElemento(this.parentNode, id)
+   })
+
+   return elementoBotao
+}
+
+function deletaElemento(tag, id) {
+   tag.remove()
+
+   itens.splice(itens.findIndex( elemento => elemento.id === id) , 1) // remover um item do array referente ao local storage
+
+   localStorage.setItem('itens', JSON.stringify(itens))
 }
