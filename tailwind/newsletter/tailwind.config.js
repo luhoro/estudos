a70377869887f6765b4fc4cfd71b7e5cea437cfa
Luisa Horowitz <118489479+luhoro@users.noreply.github.com>
2023-05-13 16:41:07 -0300

    add newsletter project (tailwind)

---
+++ tailwind/newsletter/tailwind.config.js
@@ -0,0 +1,30 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        azul: {
+          claro: "#c5dfff",
+          escuro: "#061e3c",
+          hover: "#1057b0",
+        },
+      },
+
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      keyframes: {
+        sino_kf: {
+          '0%, 100%': {
+            transform: 'rotate(-10deg)'
+          },
+          '50%' : {
+            transform: 'rotate(10deg)'
+          }
+        }
+      },
+      animation: {
+        sino: 'sino_kf 0.31s ease-in-out infinite'
+      }
+    },
+  },
+};
